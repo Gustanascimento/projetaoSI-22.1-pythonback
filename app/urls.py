a1b6bb7a74742d96c396bc356af296b5e0fe0505
Aslay Clevisson <aslayclei@gmail.com>
2022-10-02 02:13:22 -0300

    new: url para links salvos na base

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import ImageDetail, ImageList
+from .views import ImageDetail, ImageList, ImageUrl
 
 urlpatterns = [
     path('images/', ImageList.as_view(), name='image-list-and-create'),
-    path('images/<int:pk>/', ImageDetail.as_view(), name='image-detail-patch-and-destroy'),
+    path(
+        'images/<int:pk>/',
+        ImageDetail.as_view(),
+        name='image-detail-patch-and-destroy',
+    ),
+    path('images/results/', ImageUrl.as_view(), name='image-results'),
 ]
