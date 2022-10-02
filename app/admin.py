2e5f43f7dbd81f7ed7b74563f40634c26a9bd15b
Aslay Clevisson <aslayclei@gmail.com>
2022-10-02 02:09:23 -0300

    register: ImageUrl ao painel admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Image
+from .models import ImageDetail, ImageUrl
 
-admin.site.register(Image)
+admin.site.register(ImageDetail)
+admin.site.register(ImageUrl)
