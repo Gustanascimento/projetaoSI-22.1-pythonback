17972af10aeb0f181bf84fbb73364f3bd8f97187
Aslay Clevisson <aslayclei@gmail.com>
2022-10-09 20:14:07 -0300

    feat: visualização dos dados com a url exposta

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-from requests import Response
-from rest_framework import generics, status
+from rest_framework import generics
 
+from app.models.image_detail import ImageDetail
+from app.models.image_url import ImageUrl
 from app.service.request_duck_duck import DuckDuckSearch
 
-from .models import ImageDetail, ImageUrl
 # from .permissions import isAuthorOrReadOnly
 from .serializers import ImageSerializer, ImageUrlSerializer
 
@@ -14,10 +14,10 @@ def check_title_image(title):
     url_image_results = search_title_image.get_results(title)
 
     if url_image := ImageUrl.objects.filter(url=url_image_results):
-        return url_image[0].id
+        return url_image[0]
     else:
         obj = ImageUrl.objects.create(url=url_image_results)
-        return obj.id
+        return obj
 
 
 # Create your views here.
@@ -25,15 +25,15 @@ class ImageList(generics.ListCreateAPIView):
     queryset = ImageDetail.objects.all()
     serializer_class = ImageSerializer
 
-    def post(self, request, *args, **kwargs):
-        if url_image := check_title_image(request.data['title']):
-            if not request.GET._mutable:
-                request.POST._mutable = True
-
-            request.POST['image_url'] = url_image
-            return self.create(request, *args, **kwargs)
-
-        # return {'reason': 'success'}
+    def perform_create(self, serializer):
+        url = self.request.data['image_url']['url'] if 'image_url' in self.request.data else self.request.data['image_url.url']
+        
+        if url == '':
+            image_url = check_title_image(self.request.data['title'])
+        else:
+            image_url = ImageUrl.objects.create(url=self.request.data['image_url.url'])
+            
+        serializer.save(image_url=image_url)
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
