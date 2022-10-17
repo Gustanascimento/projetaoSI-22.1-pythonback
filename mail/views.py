37efc85fac0252a03a3f60925cee43799a069d80
Aslay Clevisson <aslayclei@gmail.com>
2022-10-17 20:48:45 -0300

    add: documentação para requisição de notificação de email

--- mail/views.py
+++ mail/views.py
@@ -1,6 +1,8 @@
 # from django.conf import settings
 # from django.core.mail import send_mail
 # from django.template.loader import render_to_string
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,6 +11,51 @@ from .service.mail import EmailService
 
 
 class SendEmailView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'NotificationDto': openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'category': openapi.Schema(type=openapi.TYPE_STRING),
+                        'isMarked': openapi.Schema(type=openapi.TYPE_BOOLEAN),
+                        'userData': openapi.Schema(
+                            type=openapi.TYPE_ARRAY,
+                            items=openapi.Items(
+                                type=openapi.TYPE_OBJECT,
+                                properties={
+                                    'name': openapi.Schema(
+                                        type=openapi.TYPE_STRING
+                                    ),
+                                    'email': openapi.Schema(
+                                        type=openapi.TYPE_STRING
+                                    ),
+                                },
+                            ),
+                        ),
+                        'marketProduct': openapi.Schema(
+                            type=openapi.TYPE_OBJECT,
+                            properties={
+                                'marketName': openapi.Schema(
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'productName': openapi.Schema(
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'price': openapi.Schema(
+                                    type=openapi.TYPE_NUMBER
+                                ),
+                                'quantity': openapi.Schema(
+                                    type=openapi.TYPE_NUMBER
+                                ),
+                            },
+                        ),
+                    },
+                )
+            },
+        )
+    )
     def post(self, request, format=None):
         if request.data:
             # return exception(0) or success(1)
