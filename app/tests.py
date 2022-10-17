5a0457df73735f7b5468f3e7eb6f61f1c67482f8
Aslay Clevisson <aslayclei@gmail.com>
2022-10-17 19:43:05 -0300

    fix: testes para inserção de urls na base

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from app.models.image_detail import ImageDetail
+from app.models.image_url import ImageUrl
 
 # Create your tests here.
 
@@ -8,17 +9,19 @@ from app.models.image_detail import ImageDetail
 class ImageTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.url_image = ImageUrl.objects.create(url='http://www.hojetemfrango.com.br/wp-content/uploads/2018/11/peito-de-frango-com-tomate-grape-e-azeitona-preta.jpg')
+
         cls.image = ImageDetail.objects.create(
             title='Peito de galinha',
             description='Peito de galinha com tomate',
-            image_url='http://www.hojetemfrango.com.br/wp-content/uploads/2018/11/peito-de-frango-com-tomate-grape-e-azeitona-preta.jpg',
+            image_url=cls.url_image,
         )
 
     def test_image_model(self):
         self.assertEqual(self.image.title, 'Peito de galinha')
         self.assertEqual(self.image.description, 'Peito de galinha com tomate')
         self.assertEqual(
-            self.image.image_url,
+            self.image.image_url.url,
             'http://www.hojetemfrango.com.br/wp-content/uploads/2018/11/peito-de-frango-com-tomate-grape-e-azeitona-preta.jpg',
         )
 
